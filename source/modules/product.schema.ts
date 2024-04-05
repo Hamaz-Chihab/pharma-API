f681a7478ab389e62ef7be44e56dca32c98886e1
Hamaz-Chihab <c_hamaz@estin.dz>
2024-04-05 02:14:11 +0100

    updrade the product schema

--- source/modules/product.schema.ts
+++ source/modules/product.schema.ts
@@ -4,20 +4,53 @@ interface Product {
   name: string;
   description: string;
   price: number;
-  stock_quantity: number;
-  image_url: string;
+  stockQuantity: number;
+  imageCover: string;
+  images: [string];
+  createdAt: Date;
   category?: string; // Optional
-  // Add other product-specific fields as needed
+  brand: string;
+  activeIngredients: string[];
+  dosage?: string;
+  promotions?: Promotion[];
 }
 
+interface Promotion {
+  type: string; // e.g., "discount", "bundle"
+  value: number | string; // Discount percentage or bundle details
+  startDate: Date;
+  endDate: Date;
+}
+
+const PromotionSchema = new Schema<Promotion>({
+  type: { type: String, required: true },
+  value: { type: Schema.Types.Mixed, required: true }, // Allow for numbers or descriptive text
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+});
+
 const productSchema = new Schema<Product>({
-  name: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
   description: { type: String, required: true },
-  price: { type: Number, required: true },
-  stock_quantity: { type: Number, required: true },
-  image_url: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
+  stockQuantity: { type: Number, required: true, min: 0 },
+  imageCover: {
+    type: String,
+    required: [true, "a product must have an image cover"],
+    validate: (val: string) => val.startsWith("http"),
+  },
+  createdAt: { type: Date, default: Date.now() },
   category: { type: String },
-  // Add other schema definitions for product fields
+  brand: { type: String, required: true, trim: true }, //trim is to delete the space in the begining
+  activeIngredients: [{ type: String, required: true }],
+  dosage: String,
+  promotions: [PromotionSchema],
+});
+
+productSchema.pre("save", async function (next) {
+  // Perform any actions before saving the document
+  // For example, you could normalize images URLs or create unique slugs based on the name
+  next();
 });
 
 export const ProductModel = model<Product>("Product", productSchema);
