5eafcd5fb64e4c71dddb4c191d767f488fb5f55a
Hamaz-Chihab <c_hamaz@estin.dz>
2024-04-29 22:46:54 +0100

    add auth route

--- source/controllers/authController.ts
+++ source/controllers/authController.ts
@@ -1,7 +1,8 @@
+
 import { Request, Response } from "express";
 import { UserModel } from "../modules/user.schema";
 import { catchAsync } from "../utils/catchAsync";
-export const createNewUser = catchAsync(async (req: Request, res: Response) => {
+export const signup = catchAsync(async (req: Request, res: Response) => {
   const user = await UserModel.create(req.body);
 
   // Respond with Success and User Data
--- source/routes/userRoutes.ts
+++ source/routes/userRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { createNewUser } from "../controllers/userController";
+import { signup } from "../controllers/authController";
 const userRoutes = Router();
 //error handler Middleware for the router file :
-
+// import authController from "../controllers/authController";
 //the error handler should be for the next
 //product routes :
-userRoutes.post("/", createNewUser);
+userRoutes.post("signup",signup);
+userRoutes.route("/").get().post(createNewUser);
+userRoutes.route("/:id").patch().get();
+
 export default userRoutes;
